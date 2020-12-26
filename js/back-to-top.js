303246457e0444e03d5c3a2586bb3720a506eae5
18521431@gm.uit.edu.vn <0963516672thien>
2020-12-26 23:37:10 +0700

    HTML, CSS for Information Account

---
+++ js/back-to-top.js
@@ -0,0 +1,22 @@
+
+// ====================================== Back to Top ======================================
+//Get the button
+var btnToTop = document.getElementById("btnToTop");
+console.log(btnToTop);
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        btnToTop.style.display = "block";
+    } else {
+        btnToTop.style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
--- js/index.js
+++ js/index.js
@@ -37,24 +37,3 @@ window.onclick = function(event) {
         }
     }
 }
-
-// ====================================== Back to Top ======================================
-//Get the button
-var btnToTop = document.getElementById("btnToTop");
-
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() { scrollFunction() };
-
-function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        btnToTop.style.display = "block";
-    } else {
-        btnToTop.style.display = "none";
-    }
-}
-
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
-}
