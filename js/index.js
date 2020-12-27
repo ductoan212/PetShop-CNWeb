695e89bbeda2c2450d8423c58ed49bba2c2babd3
Duc Toan <ductoan20102000@gmail.com>
2020-12-27 13:52:48 +0700

    add js for product page

--- js/index.js
+++ js/index.js
@@ -3,95 +3,95 @@ const listProduct = [{
         id: "pd-001",
         nameImg: "san_pham_1.jpg",
         name: "Temp product 1",
-        oldPrice: "120.000 VNĐ",
-        newPrice: "99.000 VNĐ"
+        oldPrice: 120,
+        newPrice: 99
     },
     {
         id: "pd-002",
         nameImg: "san_pham_2.jpg",
         name: "Temp product 2",
-        oldPrice: "100.000 VNĐ",
-        newPrice: "79.000 VNĐ"
+        oldPrice: 100,
+        newPrice: 79
     },
     {
         id: "pd-003",
         nameImg: "san_pham_3.jpg",
         name: "Temp product 3",
-        oldPrice: "1220.000 VNĐ",
-        newPrice: "700.000 VNĐ"
+        oldPrice: 1220,
+        newPrice: 700
     },
     {
         id: "pd-004",
         nameImg: "san_pham_4.jpg",
         name: "Temp product 4",
-        oldPrice: "20.000 VNĐ",
-        newPrice: "9.000 VNĐ"
+        oldPrice: 20,
+        newPrice: 9
     },
     {
         id: "pd-005",
         nameImg: "san_pham_5.jpg",
         name: "Temp product 5",
-        oldPrice: "20.000 VNĐ",
-        newPrice: "15.000 VNĐ"
+        oldPrice: 20,
+        newPrice: 15
     },
     {
         id: "pd-006",
         nameImg: "san_pham_6.jpg",
         name: "Temp product 6",
-        oldPrice: "120.000 VNĐ",
-        newPrice: "99.000 VNĐ"
+        oldPrice: 120,
+        newPrice: 99
     },
     {
         id: "pd-007",
         nameImg: "san_pham_1.jpg",
         name: "Temp product 7",
-        oldPrice: "120.000 VNĐ",
-        newPrice: "99.000 VNĐ"
+        oldPrice: 55,
+        newPrice: 23
     },
     {
         id: "pd-008",
         nameImg: "san_pham_2.jpg",
         name: "Temp product 8",
-        oldPrice: "100.000 VNĐ",
-        newPrice: "79.000 VNĐ"
+        oldPrice: 212,
+        newPrice: 199
     },
     {
         id: "pd-009",
         nameImg: "san_pham_3.jpg",
         name: "Temp product 9",
-        oldPrice: "1220.000 VNĐ",
-        newPrice: "700.000 VNĐ"
+        oldPrice: 100,
+        newPrice: 99
     },
     {
         id: "pd-010",
         nameImg: "san_pham_4.jpg",
         name: "Temp product 10",
-        oldPrice: "20.000 VNĐ",
-        newPrice: "9.000 VNĐ"
+        oldPrice: 88,
+        newPrice: 75
     },
     {
         id: "pd-011",
         nameImg: "san_pham_5.jpg",
         name: "Temp product 11",
-        oldPrice: "20.000 VNĐ",
-        newPrice: "15.000 VNĐ"
+        oldPrice: 75,
+        newPrice: 23
     },
     {
         id: "pd-012",
         nameImg: "san_pham_6.jpg",
         name: "Temp product 12",
-        oldPrice: "120.000 VNĐ",
-        newPrice: "99.000 VNĐ"
+        oldPrice: 45,
+        newPrice: 40
     },
 ]
 
 function createProduct(n) {
-    return `<div class="product-item" id="${listProduct[n]['id']}">
+    return `<div class="product-item" id="${listProduct[n]['id']}" onClick="detailProduct('${listProduct[n]['id']}')">
         <div class="img-product" style="background-image:  url(../assets/images/${listProduct[n]['nameImg']});"></div>
         <div class="info-product">
             <p class="name-product">${listProduct[n]['name']}</p>
-            <p class="old-price-product">${listProduct[n]['oldPrice']}</p>
-            <p class="new-price-product">${listProduct[n]['newPrice']}</p>
+            <p class="old-price-product">${listProduct[n]['oldPrice']}.000 VNĐ</p>
+            <p class="new-price-product">${listProduct[n]['newPrice']}.000 VNĐ</p>
         </div>
         <div class="add-to-cart">
             <button>Thêm vào giỏ</button>
@@ -104,9 +104,18 @@ function createAllProduct() {
         $('#lst-product').append(createProduct(i));
 }
 
+function getProductWithId(id) {
+    for (var i = 0; i < listProduct.length; i++) {
+        if (listProduct[i]['id'] == id)
+            return listProduct[i];
+    }
+    return null;
+}
+
 // ==================== routing navigation ==================== 
 function closeBurger() {
-    if ($(window).width() <= 1024)
+    var classList = document.getElementById('check-nav').className.split(/\s+/);;
+    if (classList.length == 2)
         $('.burger').click();
 }
 
@@ -125,6 +134,22 @@ function loadProducts() {
     document.title = "PetShop | Sản phẩm";
     createAllProduct();
     closeBurger();
+}
+
+function loadDetailProduct(id) {
+    $("#main").load("../pages/detail-product.html", function() {
+        var product = getProductWithId(id);
+        document.getElementById('img-product').style.backgroundImage = `url(../assets/images/${product['nameImg']})`;
+        document.getElementById('old-price').innerHTML = `${product['oldPrice']}.000 VNĐ`;
+        document.getElementById('new-price').innerHTML = `${product['newPrice']}.000 VNĐ`;
+        console.log(a);
+    });
+    $(".nav-links, li, a").removeClass("link-active");
+    $("#to-products").addClass("link-active");
+    document.title = "PetShop | Sản phẩm";
+    createAllProduct();
+    closeBurger();
+    topFunction();
 }
 
 function loadBlogs() {
--- js/products.js
+++ js/products.js
@@ -1,3 +1,9 @@
 $(document).ready(function() {
     createAllProduct();
 });
+
+function detailProduct(id) {
+    console.log("123");
+    console.log(id);
+    loadDetailProduct(id);
+}
