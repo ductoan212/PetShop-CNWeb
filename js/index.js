8ad3b1125fa8ebb91704185c2704da5b6e406ebb
Duc Toan <ductoan20102000@gmail.com>
2020-12-27 21:07:36 +0700

    add function add to cart

--- js/index.js
+++ js/index.js
@@ -1,122 +1,256 @@
-// ==================== Create auto product ==================== 
+// ==================== Create auto product ====================
 const listProduct = [{
         id: "pd-001",
         nameImg: "san_pham_1.jpg",
         name: "Temp product 1",
         oldPrice: 120,
-        newPrice: 99
+        newPrice: 99,
     },
     {
         id: "pd-002",
         nameImg: "san_pham_2.jpg",
         name: "Temp product 2",
         oldPrice: 100,
-        newPrice: 79
+        newPrice: 79,
     },
     {
         id: "pd-003",
         nameImg: "san_pham_3.jpg",
         name: "Temp product 3",
         oldPrice: 1220,
-        newPrice: 700
+        newPrice: 700,
     },
     {
         id: "pd-004",
         nameImg: "san_pham_4.jpg",
         name: "Temp product 4",
         oldPrice: 20,
-        newPrice: 9
+        newPrice: 9,
     },
     {
         id: "pd-005",
         nameImg: "san_pham_5.jpg",
         name: "Temp product 5",
         oldPrice: 20,
-        newPrice: 15
+        newPrice: 15,
     },
     {
         id: "pd-006",
         nameImg: "san_pham_6.jpg",
         name: "Temp product 6",
         oldPrice: 120,
-        newPrice: 99
+        newPrice: 99,
     },
     {
         id: "pd-007",
         nameImg: "san_pham_1.jpg",
         name: "Temp product 7",
         oldPrice: 55,
-        newPrice: 23
+        newPrice: 23,
     },
     {
         id: "pd-008",
         nameImg: "san_pham_2.jpg",
         name: "Temp product 8",
         oldPrice: 212,
-        newPrice: 199
+        newPrice: 199,
     },
     {
         id: "pd-009",
         nameImg: "san_pham_3.jpg",
         name: "Temp product 9",
         oldPrice: 100,
-        newPrice: 99
+        newPrice: 99,
     },
     {
         id: "pd-010",
         nameImg: "san_pham_4.jpg",
         name: "Temp product 10",
         oldPrice: 88,
-        newPrice: 75
+        newPrice: 75,
     },
     {
         id: "pd-011",
         nameImg: "san_pham_5.jpg",
         name: "Temp product 11",
         oldPrice: 75,
-        newPrice: 23
+        newPrice: 23,
     },
     {
         id: "pd-012",
         nameImg: "san_pham_6.jpg",
         name: "Temp product 12",
         oldPrice: 45,
-        newPrice: 40
+        newPrice: 40,
     },
-]
+];
 
 function createProduct(n) {
-    return `<div class="product-item" id="${listProduct[n]['id']}" onClick="detailProduct('${listProduct[n]['id']}')">
-        <div class="img-product" style="background-image:  url(../assets/images/${listProduct[n]['nameImg']});"></div>
-        <div class="info-product">
-            <p class="name-product">${listProduct[n]['name']}</p>
-            <p class="old-price-product">${listProduct[n]['oldPrice']}.000 VNĐ</p>
-            <p class="new-price-product">${listProduct[n]['newPrice']}.000 VNĐ</p>
+    return `<div class="product-item" id="${listProduct[n]["id"]}" >
+        <div onClick="detailProduct('${listProduct[n]["id"]}')">
+            <div class="img-product" style="background-image:  url(../assets/images/${listProduct[n]["nameImg"]});"></div>
+            <div class="info-product">
+                <p class="name-product">${listProduct[n]["name"]}</p>
+                <p class="old-price-product">${listProduct[n]["oldPrice"]}.000 VNĐ</p>
+                <p class="new-price-product">${listProduct[n]["newPrice"]}.000 VNĐ</p>
+            </div>
         </div>
         <div class="add-to-cart">
-            <button>Thêm vào giỏ</button>
+            <button id="btn-${listProduct[n]["id"]}" onclick="addToCart('${listProduct[n]["id"]}')">Thêm vào giỏ</button>
         </div>
     </div>`;
 }
 
 function createAllProduct() {
-    for (var i = 0; i < 12; i++)
-        $('#lst-product').append(createProduct(i));
+    for (var i = 0; i < 12; i++) $("#lst-product").append(createProduct(i));
 }
 
 function getProductWithId(id) {
     for (var i = 0; i < listProduct.length; i++) {
-        if (listProduct[i]['id'] == id)
-            return listProduct[i];
+        if (listProduct[i]["id"] == id) return listProduct[i];
     }
     return null;
 }
 
-// ==================== routing navigation ==================== 
+//==================== Store list item in cart ====================
+var listCart = [];
+
+// {
+//     info: {
+//         id: "pd-001",
+//         nameImg: "san_pham_1.jpg",
+//         name: "Temp product 1",
+//         oldPrice: 120,
+//         newPrice: 99
+//     },
+//     quantity: 10,
+//     sum: 990
+// },
+// {
+//     info: {
+//         id: "pd-002",
+//         nameImg: "san_pham_2.jpg",
+//         name: "Temp product 2",
+//         oldPrice: 100,
+//         newPrice: 79
+//     },
+//     quantity: 10,
+//     sum: 790
+// }
+
+function createItemCart(item) {
+    return `<div class="item-product" id="${item["info"]["id"]}">
+                <div class="img-product" style="background-image:  url(../assets/images/${item["info"]["nameImg"]});"></div>
+                <div class="info-product">
+                    <div class="name-price">
+                        <p class="name-product">${item["info"]["name"]}</p>
+                        <p class="price-product">${item["info"]["newPrice"]}.000 VNĐ</p>
+                    </div>
+                    <div class="quantity-product">
+                        <input id="qua-${item["info"]["id"]}" type="number" min="1" value="${item["quantity"]}">
+                    </div>
+                    <div id="sum-${item["info"]["id"]}" class="sum-price-product">${item["sum"]}.000 VNĐ</div>
+                </div>
+                <div class="delete-product">
+                    <img src="../assets/icons/remove.png" id="img-${item["info"]["id"]}" onclick="removeItemOnCart('${item["info"]["id"]}')">
+                </div>
+            </div>`;
+}
+
+function createAllCart() {
+    for (var i = 0; i < listCart.length; i++) {
+        $("#list-cart").append(createItemCart(listCart[i]));
+        console.log(createItemCart(listCart[i]));
+    }
+}
+
+function calculateTotal() {
+    var s = 0;
+    for (var i = 0; i < listCart.length; i++) {
+        var temp = listCart[i]["info"]["newPrice"] * listCart[i]["quantity"];
+        listCart[i]["sum"] = temp;
+        s += temp;
+    }
+    console.log(s);
+    return s;
+}
+
+function updateTotal() {
+    for (var i = 0; i < listCart.length; i++) {
+        temp = document.getElementById(listCart[i]["info"]["id"]);
+        var quantity = $(`#qua-${listCart[i]["info"]["id"]}`).val();
+        if (quantity < 1) {
+            quantity = 1;
+            $(`#qua-${listCart[i]["info"]["id"]}`).val(1);
+        }
+        var sum = listCart[i]["info"]["newPrice"] * quantity;
+        listCart[i]["quantity"] = parseInt(quantity);
+        listCart[i]["sum"] = parseInt(sum);
+        // console.log(listCart[i]);
+        $(`#sum-${listCart[i]["info"]["id"]}`).text(`${sum}.000 VNĐ`);
+    }
+    // loadCart();
+    console.log("listCart");
+    document.getElementById(
+        "total"
+    ).innerHTML = `Tổng tiền: ${calculateTotal()}.000 VNĐ`;
+}
+
+function checkout() {
+    listCart = [];
+    alert(
+        "Cảm ơn bạn đã mua hàng. Sau khi thanh toán toàn bộ sản phẩm trong giỏ hàng sã bị xóa"
+    );
+    loadCart();
+}
+
+// ==================== add to cart ====================
+function addToCart(id) {
+    console.log(id);
+    const product = {
+        info: getProductWithId(id),
+        quantity: 1,
+        sum: 0,
+    };
+    // Kiem tra xem gio hang co chua
+    var i = 0;
+    while (i < listCart.length) {
+        if (listCart[i]["info"]["id"] == id) break;
+        i++;
+    }
+    console.log(i);
+    if (i == listCart.length) {
+        listCart.push(product);
+    } else {
+        listCart[i]["quantity"]++;
+    }
+}
+
+function removeItemOnCart(id) {
+    console.log(id);
+    // Tim vi tri cua item
+    var i = 0;
+    while (i < listCart.length) {
+        if (listCart[i]["info"]["id"] == id) break;
+        i++;
+    }
+    if (i == listCart.length) return;
+    // if (listCart.length == 1)
+    //     listCart = [];
+    console.log(i);
+    temp1 = listCart.slice(0, i);
+    temp2 = listCart.slice(i + 1, listCart.length);
+    listCart = temp1.concat(temp2);
+    console.log(listCart);
+    // console.log(temp1);
+    // console.log(temp2);
+    loadCart();
+}
+
+// ==================== routing navigation ====================
 function closeBurger() {
-    var classList = document.getElementById('check-nav').className.split(/\s+/);;
-    if (classList.length == 2)
-        $('.burger').click();
+    var classList = document.getElementById("check-nav").className.split(/\s+/);
+    if (classList.length == 2) $(".burger").click();
 }
 
 function loadHome() {
@@ -139,9 +273,19 @@ function loadProducts() {
 function loadDetailProduct(id) {
     $("#main").load("../pages/detail-product.html", function() {
         var product = getProductWithId(id);
-        document.getElementById('img-product').style.backgroundImage = `url(../assets/images/${product['nameImg']})`;
-        document.getElementById('old-price').innerHTML = `${product['oldPrice']}.000 VNĐ`;
-        document.getElementById('new-price').innerHTML = `${product['newPrice']}.000 VNĐ`;
+        document.getElementById(
+            "img-product"
+        ).style.backgroundImage = `url(../assets/images/${product["nameImg"]})`;
+        document.getElementById("name-product").innerHTML = product["name"];
+        document.getElementById(
+            "old-price"
+        ).innerHTML = `${product["oldPrice"]}.000 VNĐ`;
+        document.getElementById(
+            "new-price"
+        ).innerHTML = `${product["newPrice"]}.000 VNĐ`;
+        document
+            .getElementById("btn-add-to-cart")
+            .setAttribute("onclick", `addToCart('${product["id"]}')`);
         console.log(a);
     });
     $(".nav-links, li, a").removeClass("link-active");
@@ -165,6 +309,7 @@ function loadDetailBlog() {
     $(".nav-links, li, a").removeClass("link-active");
     $("#to-blogs").addClass("link-active");
     document.title = "PetShop | Bài viết";
+    topFunction();
     closeBurger();
 }
 
@@ -191,35 +336,44 @@ function loadAccount() {
 }
 
 function loadCart() {
-    $("#main").load("../pages/cart.html");
-    $(".nav-links, li, a").removeClass("link-active");
     document.title = "PetShop | Giỏ hàng";
+    if (listCart.length == 0) {
+        $("#main").load("../pages/cart-empty.html");
+    } else {
+        $("#main").load("../pages/cart.html");
+    }
+    $(".nav-links, li, a").removeClass("link-active");
+    // createAllCart();
+    document.getElementById(
+        "total"
+    ).innerHTML = `Tổng tiền: ${calculateTotal()}.000 VNĐ`;
 }
 
 $(document).ready(function() {
-    loadHome();
+    // loadHome();
+    loadProducts();
 });
 
 // ====================================== Nav ======================================
 const navSlide = () => {
-    const burger = document.querySelector('.burger');
-    const nav = document.querySelector('.nav-links');
-    const navLinks = document.querySelectorAll('.nav-links li');
-    burger.addEventListener('click', () => {
+    const burger = document.querySelector(".burger");
+    const nav = document.querySelector(".nav-links");
+    const navLinks = document.querySelectorAll(".nav-links li");
+    burger.addEventListener("click", () => {
         //Toggle Nav
-        nav.classList.toggle('nav-active');
+        nav.classList.toggle("nav-active");
         //Animate links
         navLinks.forEach((link, index) => {
             if (link.style.animation) {
-                link.style.animation = '';
+                link.style.animation = "";
             } else {
-                link.style.animation = `navLinkFade 0.5s ease forwards ${index/5}s`;
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 5}s`;
             }
         });
         //Burger animation
-        burger.classList.toggle('toggle');
+        burger.classList.toggle("toggle");
     });
-}
+};
 navSlide();
 
 // ====================================== Drop down for account zone ======================================
@@ -229,34 +383,32 @@ function dropdownAccount() {
 
 // Close the dropdown menu if the user clicks outside of it
 window.onclick = function(event) {
-    if (!event.target.matches('.account-btn')) {
+    if (!event.target.matches(".account-btn")) {
         var dropdowns = document.getElementsByClassName("dropdown-content");
         var i;
         for (i = 0; i < dropdowns.length; i++) {
             var openDropdown = dropdowns[i];
-            if (openDropdown.classList.contains('show')) {
-                openDropdown.classList.remove('show');
+            if (openDropdown.classList.contains("show")) {
+                openDropdown.classList.remove("show");
             }
         }
     }
-}
+};
 
 // Slide show
 var counter = 1;
 setInterval(function() {
-    document.getElementById('radio' + counter).checked = true;
+    document.getElementById("radio" + counter).checked = true;
     counter++;
     if (counter > 4) {
         counter = 1;
     }
 }, 5000);
 
-
 function clearSubcribeForm() {
-    $('#emailSubcribe').val('');
+    $("#emailSubcribe").val("");
 }
 
-
 function clearSearch() {
-    $('#search').val('');
+    $("#search").val("");
 }
